b1b164116dda634ca6f1e50eb5d7057bab3e6501
Madli <madli.petuhhov89@gmail.com>
2024-04-17 13:55:55 +0300

    new main event WIP

--- src/main/java/ee/valiit/suvepiduback/summerevent/business/BusinessController.java
+++ src/main/java/ee/valiit/suvepiduback/summerevent/business/BusinessController.java
@@ -1,9 +1,12 @@
 package ee.valiit.suvepiduback.summerevent.business;
 
+import ee.valiit.suvepiduback.summerevent.business.dto.BusinessesDropdownInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -12,8 +15,8 @@ public class BusinessController {
     @GetMapping("/businesses")
     @Operation(summary = "Ettevõtete (companyName) valiku loomine. Tagastab businessId.",
             description = "Süsteemist otsitakse businessId abil ettevõtted (companyName).")
-    public void executeBusinessesDropdown() {
-        businessService.executeBusinessesDropdown();
+    public List<BusinessesDropdownInfo> executeBusinessesDropdown() {
+        return businessService.executeBusinessesDropdown();
     }
 
 }
--- src/main/java/ee/valiit/suvepiduback/summerevent/business/BusinessService.java
+++ src/main/java/ee/valiit/suvepiduback/summerevent/business/BusinessService.java
@@ -1,9 +1,13 @@
 package ee.valiit.suvepiduback.summerevent.business;
 
+import ee.valiit.suvepiduback.domain.account.business.Business;
 import ee.valiit.suvepiduback.domain.account.business.BusinessMapper;
 import ee.valiit.suvepiduback.domain.account.business.BusinessRepository;
+import ee.valiit.suvepiduback.summerevent.business.dto.BusinessesDropdownInfo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -11,7 +15,9 @@ public class BusinessService {
 
     private final BusinessRepository businessRepository;
     private final BusinessMapper businessMapper;
-    public void executeBusinessesDropdown() {
-        businessRepository.findAll();
+    public List<BusinessesDropdownInfo> executeBusinessesDropdown() {
+        List<Business> businesses = businessRepository.findAll();
+        return businessMapper.toBusinessesDropdownInfos(businesses);
+
     }
 }
