ce47404d00dcd2dcf5b53ef44d9b30eb01aaa766
Madli <madli.petuhhov89@gmail.com>
2024-04-17 11:51:08 +0300

    new main event WIP

--- database/3_import.sql
+++ database/3_import.sql
@@ -2,7 +2,8 @@ INSERT INTO suvepidu.role (id, name) VALUES (default, 'tavakasutaja');
 INSERT INTO suvepidu.role (id, name) VALUES (default, 'korraldaja');
 
 INSERT INTO suvepidu."user" (id, role_id, username, password, status) VALUES (default, 1, 'maarek', '123', 'A');
-INSERT INTO suvepidu."user" (id, role_id, username, password, status) VALUES (default, 2, 'admin', '123', 'A');
+INSERT INTO suvepidu."user" (id, role_id, username, password, status) VALUES (default, 2, 'korraldaja', '123', 'A');
 INSERT INTO suvepidu."user" (id, role_id, username, password, status) VALUES (default, 1, 'rain', '123', 'D');
 
 INSERT INTO suvepidu.business (id, user_id, company_name, phone, email, registry_code, vat_number, status) VALUES (default, 2, 'maarek OÜ', '123', 'maarek@maarek.ee', '123', '123', 'A');
+INSERT INTO suvepidu.business (id, user_id, company_name, phone, email, registry_code, vat_number, status) VALUES (default, 2, 'riigo OÜ', '321', 'riigo@riigo.ee', '321', '321', 'A');
