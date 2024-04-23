4ed90aa2fb3e9ea95cf6f69fb5efb0ebf52e92f7
test <test@test>
2024-04-23 16:20:38 +0300

    SUVEPIDU-EVENT-TICKETS-WIP003

--- src/main/java/ee/valiit/suvepiduback/summerevent/ticket/TicketController.java
+++ src/main/java/ee/valiit/suvepiduback/summerevent/ticket/TicketController.java
@@ -3,6 +3,7 @@ package ee.valiit.suvepiduback.summerevent.ticket;
 import ee.valiit.suvepiduback.summerevent.ticket.dto.EventTicketInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +20,12 @@ public class TicketController {
     public void addNewTicket(@RequestBody EventTicketInfo eventTicketInfo) {
         ticketService.addNewTicket(eventTicketInfo);
     }
+
+    @GetMapping("/event/ticket")
+    @Operation(summary = "Piletiinfo toomine andmebaasist",
+            description = "Andmebaasist tuuakse ühe pileti info EventTicketId abil, kui selle staatus on aktiivne.")
+    public void getEventTicket() {
+        ticketService.getEventTicket();
+    }
+
 }
--- src/main/java/ee/valiit/suvepiduback/summerevent/ticket/TicketService.java
+++ src/main/java/ee/valiit/suvepiduback/summerevent/ticket/TicketService.java
@@ -35,4 +35,7 @@ public class TicketService {
 
     }
 
+    public void getEventTicket() {
+
+    }
 }
